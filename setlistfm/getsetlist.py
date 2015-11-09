313fe581d08134564ecc7125188831153fdc90c0
camkruse <camkruse@gmail.com>
2015-11-08 21:10:26 -0500

    Change to Class

--- setlistfm/getsetlist.py
+++ setlistfm/getsetlist.py
@@ -1,23 +1,51 @@
+# 
+# Created By Cameron Krusemark
+# Created 11/8/2015
 
+class setlist:
+    def getSetlistByArtist(self, artist):
+        import json, requests
 
-# URL library
-import json, requests, pprint
+        # Get method parameter and build web service URL   
+        #self.artist = artist  
+        url = "http://api.setlist.fm/rest/0.1/search/setlists.json?artistName="+artist
+        
+        # Call Setlist.fm API using requests library
+        resp = requests.get(url)
+        
+        # Format Request into JSON Object
+        self.data = json.loads(resp.text)
+        
+    def getSetlistByArtistMbid(self, artistMbid):
+        import json, requests
 
-url = 'http://api.setlist.fm/rest/0.1/search/setlists.json?artistName=metallica'
-
-"""
-params = dict(
-    origin='Chicago,IL',
-    destination='Los+Angeles,CA',
-    waypoints='Joplin,MO|Oklahoma+City,OK',
-    sensor='false'
-)
-"""
-#resp = requests.get(url=url, params=params)
-resp = requests.get(url=url)
-data = json.loads(resp.text)
-
-with open('metallica.txt', 'w') as outfile: 
-        json.dump(data, outfile)
+        # Get method parameter and build web service URL     
+        url = "http://api.setlist.fm/rest/0.1/search/setlists.json?artistMbid="+artistMbid
+        
+        # Call Setlist.fm API using requests library
+        resp = requests.get(url)
+        
+        # Format Request into JSON Object
+        self.data = json.loads(resp.text)
     
- # pprint.pprint(data)
+####################################################
+# Below will be moved into the processing package
+
+# Create Object from Class
+objSetlistbyArtist = setlist()
+
+# Call Method from Object
+objSetlistbyArtist.getSetlistByArtist('Metallica')
+
+# Print JSON
+print objSetlistbyArtist.data
+
+
+# Example 2
+objSetlistByArtistMbid = setlist()
+objSetlistByArtistMbid.getSetlistByArtistMbid('a16e47f5-aa54-47fe-87e4-bb8af91a9fdd')
+print objSetlistByArtistMbid.data
+
+
+
+
